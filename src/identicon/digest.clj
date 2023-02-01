f9e941366eb9cf89443f0d792a90e53bcf8fdf76
lim6112j <lim6112j@gmail.com>
2023-02-02 02:29:11 +0900

    added digest

---
+++ src/identicon/digest.clj
@@ -0,0 +1,11 @@
+(ns identicon.digest
+(:import java.security.MessageDigest)
+(:import java.math.BigInteger)
+)
+(defn md5 [s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        size (* 2 (.getDigestLength algorithm))
+        raw (.digest algorithm (.getBytes s))
+        sig (.toString (BigInteger. 1 raw) 16)
+        padding (apply str (repeat (- size (count sig)) "0"))]
+    (str padding sig)))
