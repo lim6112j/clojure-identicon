12be8d1fdf204915cd2855ad66b68d3efa3a8b47
lim6112j <lim6112j@gmail.com>
2023-02-02 02:29:25 +0900

    added digest

--- src/identicon/core.clj
+++ src/identicon/core.clj
@@ -1,11 +1,13 @@
 (ns identicon.core
-  (:require [digest])
+  (:gen-class)
+  (:require [digest :refer [md5]])
   (:require [clojure.string :as str])
-  (import java.io.File)
-  (import java.awt.Color)
-  (import java.awt.image.BufferedImage)
-  (import javax.imageio.ImageIO)
+  (:import java.io.File)
+  (:import java.awt.Color)
+  (:import java.awt.image.BufferedImage)
+  (:import javax.imageio.ImageIO)
   )
+;;(use 'identicon.digest)
 (def tiles-per-side 6)
 (def total-tiles (* tiles-per-side tiles-per-side))
 (defn- get-color
@@ -28,6 +30,104 @@
     (244 154 193) ;; Pastel Magenta
     (245 152 157) ;; Pastel Magenta Red
          ), pos)
+  )
+(defn- from-hex-dec
+  "Convert a hex char to an int"
+  [character]
+  (cond
+    (= "a" character) 10
+    (= "b" character) 11
+    (= "c" character) 12
+    (= "d" character) 13
+    (= "e" character) 14
+    (= "f" character) 15
+    :else (Integer/parseInt character)
+    )
+  )
+(defn- to-numbers
+  "Convert a string of hex chars to a seq of ints"
+  [num_string]
+  (map from-hex-dec (rest (clojure.string/split num_string #"")))
+  )
+(defn- to-bools
+  "Formulically convert a seq of ints to bools"
+  [num_string]
+  (take (/ total-tiles 2) (cycle
+    (map #(> % 7) (to-numbers num_string))))
+  )
+(defn- in-row
+  "Return the row for a particular position in the seq"
+  [pos]
+  (quot pos (/ tiles-per-side 2))
+  )
+(defn- in-col
+  "Returns the column for a particular position in the seq"
+  [pos]
+  (rem pos (/ tiles-per-side 2))
+  )
+(defn- draw-tile
+  "Fill in a tile at a particula position starting from the left of the image"
+  [draw tile-size pos]
+  (.fillRect draw
+    (* (in-col pos) tile-size)
+    (* (in-row pos) tile-size)
+    tile-size tile-size
+  )
+  )
+(defn- draw-mirror-tile
+  "Fill in a tile at a particular position starting from the right of the image"
+  [draw tile-size pos]
+  (.fillRect draw
+             (* (- tiles-per-side (in-col pos) 1) tile-size)
+             (* (in-row pos) tile-size)
+             tile-size tile-size
+             )
+  )
+(defn- draw-it
+  "Draw tiles on the image based on a seq of booleans to determine whether the tile is filled in or not"
+  [draw tile-size pos bools]
+  (do
+    (if (first bools)
+      (do
+        ;; left half
+        (draw-tile draw tile-size pos)
+        ;; right half
+        (draw-mirror-tile draw tile-size pos)
+        )
+      )
+    )
+  )
+(defn- file-name
+  "Generate a file name"
+  [name size]
+  (str/join "" [name "." size ".png"])
+  )
+(defn- fill-background
+  "Fill in the background with white"
+  [draw size]
+  (do
+    (.setColor draw (Color/WHITE))
+    (.fillRect draw 0 0 size size)
+    )
+  )
+(defn- generate
+  "Make a new avatar"
+  [identifier size]
+  (let
+      [tile-size (quot size tiles-per-side)
+       md5 (digest/md5 identifier)
+       icon (BufferedImage. size size BufferedImage/TYPE_INT_RGB)
+       [r g b] (get-color (first (to-numbers md5)))
+       color (Color. r g b)
+       draw (.createGraphics icon)
+       ]
+    (do
+      (fill-background draw size)
+      (.setColor draw color)
+      (draw-it draw tile-size 0 (to-bools md5))
+      (ImageIO/write icon "png" (File. (file-name md5 size)))
+      )
+      )
   )
 (defn -main
   "I don't do a whole lot ... yet."
